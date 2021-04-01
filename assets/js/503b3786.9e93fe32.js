42eb83c6e90f2b884b8aec53811be23a09b001f8
ahmadhafizh223 <ahmadhafizh223@gmail.com>
2021-04-02 06:10:27 +0700

    Deploy website - based on 9000fa1af18859f7b89507fbd5ac68bba78e6b3f

---
+++ assets/js/503b3786.9e93fe32.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{86:function(o){o.exports=JSON.parse('{"allTagsPath":"/documee/blog/tags","slug":"facebook","name":"facebook","count":1,"permalink":"/documee/blog/tags/facebook"}')}}]);
---
+++ assets/js/a8aa5537.09028c28.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[22],{96:function(o){o.exports=JSON.parse('{"allTagsPath":"/documee/blog/tags","slug":"hola","name":"hola","count":1,"permalink":"/documee/blog/tags/hola"}')}}]);
